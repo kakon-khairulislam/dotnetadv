1eb60fdbb7ae7dd5ffa995af7558c1432324432c
kakon-khairulislam <99829285+kakon-khairulislam@users.noreply.github.com>
2023-06-12 04:34:21 +0600

    first lab task

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using labtask1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace labtask1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult welcome()
+        {
+            ViewBag.CompanyName = "Fidelity Investments";
+            ViewBag.shortinfo = "Your fund is safe with us";
+            return View();
+        }
+        public ActionResult aboutus()
+        {
+            return View(); 
+        }
+        public ActionResult teamandmembers()
+        {
+            var e1 = new Models.teamandmembers()
+            {
+                Position = "CEO",
+                Name = "Mr. Donald Krump",
+                Salary = "2000$"
+
+            };
+            var e2 = new Models.teamandmembers()
+            {
+                Position = "Manager",
+                Name = "Sheikh Lasina",
+                Salary = "500$"
+            };
+            var e3 = new teamandmembers()
+            {
+                Position = "Intern",
+                Name = "Naren Modi",
+                Salary = "50$"
+            };
+            teamandmembers[] eds = new teamandmembers[] { e1, e2, e3 };
+
+            return View(eds);
+        }
+        public ActionResult contactus()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+
+    
+}
---
+++ Models/teamandmembers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace labtask1.Models
+{
+    public class teamandmembers
+    {
+        public string Position { get; set; }
+        public string Name { get; set; }
+        public string Salary { get; set; }
+    }
+}
